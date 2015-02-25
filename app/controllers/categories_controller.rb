8973614699e83af4b87725c9725676aee7e67f6d
Steven Cassidy <steven@mumsnet.com>
2015-02-24 21:59:40 -0500

    Add category maintenance form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,23 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def new
+    #this displays the new category form
+    @category = Category.new
+  end
+
+  def create
+    #this will get called when we post a new category
+  end
+
+  def edit
+    #this displays the new category form
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    #this gets called when we PATCH/PUT the category from the form
+
+  end
+
 end
