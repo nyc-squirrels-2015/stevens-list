0334dcfcca8327c6c2395155dac1262fdaa2451c
stevecass <steven@mumsnet.com>
2015-03-04 15:59:47 -0500

    Add a welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :categories do
     resources :articles, except: [:index]
   end
+  root 'categories#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
